b639824b2d4449c5b461b280c2df0fd2a40f8258
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-01-09 11:07:34 -0800

    gallery

--- gallery/upload_image_config.php
+++ gallery/upload_image_config.php
@@ -1,28 +1,18 @@
 <?php
-   $servername ="localhost";
-   $user = "root";
-   $pass="";
-   $db = "demo";
+$hostname='localhost';
+$user='root';
+$pass='';
+$db='demo';
 
-   $conn = mysqli_connect($servername,$user,$pass,$db);
-   if(!$conn)
-   {
-       die("error detected".mysqli_error($conn));
-   }else{
-      
-      echo 'Connected';
-      
-   }
+$conn = mysqli_connect($hostname,$user,$pass,$db);
+if(!$conn){
+   die('Connection Failed: '.mysqli_connect_error());
+}
+echo 'Connected';
 
+$image = $_FILES['image']['tmp_name'];
+$imgdata = addslashes(file_get_contents($image));
 
-   $image = $_FILES['image']['tmp_name'];
-   $imgdata = addslashes(file_get_contents($image));
+$sql=mysqli_connect($conn,"INSERT INTO gallery(image)VALUES('$imgdata')");
 
-   $sql = mysqli_query($conn,"INSERT INTO gallery(image)VALUES('$imgdata')");
-
-   if($conn->query($sql) === True){
-      echo 'Image upload successfully';
-   }else{
-      echo 'Error:'.$conn->error;  
-   }
 ?>
